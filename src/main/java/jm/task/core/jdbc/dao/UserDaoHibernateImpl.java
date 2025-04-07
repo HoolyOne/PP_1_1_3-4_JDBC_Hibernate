ceff1570b116b108923911bbc5695ae719e0ad1e
HoolyOne <sanya.pyro@gmail.com>
2025-04-07 20:22:35 +0300

    Задание 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,9 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -12,31 +15,66 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        try (Session session = Util.getHibernateSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.createNativeQuery("CREATE TABLE IF NOT EXISTS users (" +
+                    "id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                    "name VARCHAR(255), " +
+                    "lastName VARCHAR(255), " +
+                    "age TINYINT)").executeUpdate();
+            tx.commit();
+        }
 
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = Util.getHibernateSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            tx.commit();
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        User user = new User(name, lastName, age);
+        try (Session session = Util.getHibernateSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.save(user);
+            tx.commit();
+        }
 
     }
 
     @Override
     public void removeUserById(long id) {
+        try (Session session = Util.getHibernateSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            User user = (User) session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
+                tx.commit();
+            }
+        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getHibernateSessionFactory().openSession()) {
+            return session.createQuery("FROM User", User.class).getResultList();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.getHibernateSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.createNativeQuery("TRUNCATE TABLE users").executeUpdate();
+            tx.commit();
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,6 +16,8 @@ public class Util {
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "3325";
 
+    private static SessionFactory sessionFactory;
+
     public static Connection getConnection() {
         Connection con = null;
         try {
@@ -21,5 +28,27 @@ public class Util {
             throw new RuntimeException("Ошибка подключения к БД", e);
         }
         return con;
+    }
+    public static SessionFactory getHibernateSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                configuration.setProperty("hibernate.connection.driver_class", DB_DRIVER);
+                configuration.setProperty("hibernate.connection.url", DB_URL);
+                configuration.setProperty("hibernate.connection.username", DB_USER);
+                configuration.setProperty("hibernate.connection.password", DB_PASSWORD);
+                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                configuration.setProperty("hibernate.hdm2ddl.auto", "update");
+                configuration.setProperty("hibernate.show_sql", "true");
+                configuration.addAnnotatedClass(User.class);
+
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e) {
+                System.out.println("Ошибка при создании SessionFactory" + e);
+            }
+        }
+        return sessionFactory;
     }
 }
